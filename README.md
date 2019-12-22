f51f45677e85507dbc1bb37c22f8b19413c71b58
oc-rajeshu <rajesh.upadhayaya@creditsafe.co.in>
2019-12-23 00:17:55 +0530

    update requirement file

---
+++ README.md
@@ -0,0 +1,7 @@
+Online Exam Platform using Python Django
+
+**How to run?**
+
+1. Run `pip install requirements.txt` To install all dependencies.
+2. Run `python manage.py migrate` to create database tables.
+3. Run `python manage.py runserver` to run the project.
--- onlineexam/settings.py
+++ onlineexam/settings.py
@@ -79,21 +79,21 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': 'createonlineexam',
-        'USER': 'root',
-        'PASSWORD': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'NAME': 'createonlineexam',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#     }
+# }
 
 
 # Password validation
